1e905ec499346819215a15721d136999a3d6c603
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2023-08-22 20:58:35 +0600

    First

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+st.title('AzimHealth AI Fitness Trainer')
+
+
+#displaying a local video file
+
+
+# Define the URL of the gif
+gif_url = "output_recorded.gif"
+
+# Display the gif using st.image
+st.image(gif_url)
+
+    
+    
+
+    
+
+
+
+    
+    
+
+    
+
+
